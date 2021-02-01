8bbb943155cd93034c1ef8b2c8e2bafd69661556
hufan <hufan@qding.me>
2021-02-01 13:56:18 +0800

    修改邮箱

--- weather.sh
+++ weather.sh
@@ -7,12 +7,11 @@ LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
+rm result*.html
+curl rate.sx/?qFT > result0.html
+cat result0.html |grep "^│"|awk -F "│" '{print $3 $4 $5 $6}' > result1.html
+sed -i 's/Coin .*/  Coin     Price      Change(24H)   (1H)/g' result1.html
 
-curl \
-    -H "Accept-Language: $LANGUAGE" \
-    -H "User-Agent: $UA" \
-    -o result.html \
-    rate.sx/eth
 
 #wttr.in/$CITY
 #wttr.in/$CITY?format=4\&$UNIT
@@ -20,7 +19,13 @@ curl \
 
 CHAT_ID=$1
 BOT_TOKEN=$2
-SEND_TEXT=`cat result.html`
+
+d=`date --utc "+%Y-%m-%d %H:%M:%SZ"`
+curl -k \
+     --data chat_id="$CHAT_ID" \
+     --data "text=$d" "https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
+
+SEND_TEXT=`cat result1.html`
 
 curl -k \
      --data chat_id="$CHAT_ID" \
